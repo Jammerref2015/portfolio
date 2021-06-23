50b1a7fbab876bfc9950e5220a40f8a114df5849
Jammerref2015 <adrian.looney@gmail.com>
2021-06-23 14:01:17 +0100

    initial home page created

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from 'react';
+import {faFacebook, faYoutube, faGithub} from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {Link} from 'react-router-dom';
 
 function HomePage() {
     return (
-        <div>
-            <h1>Home Page</h1>
+        <div className="HomePage">
+            <header className="hero">
+                <h1 className="hero-text">
+                    Hi, I am 
+                    <span> Adrian Looney</span>
+                </h1>
+                <p className="h-sub-text"> 
+                    Random placeholder text. Random placeholder text. Random placeholder text. Random placeholder text.
+                    Random placeholder text.Random placeholder text.Random placeholder text.Random placeholder text.
+                </p>
+                <div className="icons">
+                    <Link className="icon-holder">
+                        <FontAwesomeIcon icon={faFacebook} className="icon fb" />
+                    </Link>
+                    <Link className="icon-holder">
+                        <FontAwesomeIcon icon={faGithub} className="icon gh"/>
+                    </Link>
+                    <Link className="icon-holder">
+                        <FontAwesomeIcon icon={faYoutube} className="icon yt" />
+                    </Link>
+                </div>
+            </header>
         </div>
     )
 }
