dc13f56f3d33c2b9c8c2f6753f89cbbe28f9da23
Jammerref2015 <adrian.looney@gmail.com>
2021-07-11 12:02:27 +0100

    created initial blogs page

--- src/pages/BlogsPage.js
+++ src/pages/BlogsPage.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import allBlogs from '../components/allBlogs';
+import Title from '../components/Title'
 
 function BlogsPage() {
     return (
         <div>
-            <h1>Blogs page</h1>
+            <div className="b-title">
+                <Title title= {'Recent Blogs'} span={'Recent Blogs'} />
+            </div>
+            <div className="BlogsPage">
+            {
+                allBlogs.map((blog)=> {
+                    return <div className="blog" key={blog.id}>
+                        <div className="blog-content">
+                            <img src={blog.image} alt=""/>
+                            <a href={blog.link} className="blog-link" target="_blank" rel="noreferrer">
+                                {blog.title}
+                            </a>
+                        </div> 
+                    </div>
+                })
+            }
+            </div>
         </div>
     )
 };
