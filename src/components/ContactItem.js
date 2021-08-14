caa0bdbadfd8405dd7129353596b9cbb896c505b
Jammerref2015 <adrian.looney@gmail.com>
2021-08-14 16:02:21 +0100

    Correction made to contact page - typo

--- src/components/ContactItem.js
+++ src/components/ContactItem.js
@@ -5,7 +5,7 @@ function ContactItem({icon, text1, text2, title}) {
         <div className="ContactItem">
             <div className="contact">
                 <img src={icon} alt="" />
-                <div className="right-item">
+                <div className="right-items">
                     <h6>{title}</h6>
                     <p>{text1}</p>
                     <p>{text2}</p>
