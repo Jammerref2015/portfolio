8c8f6b8d4c0e5b05017d080d61578f92ca069242
Jammerref2015 <adrian.looney@gmail.com>
2021-08-23 15:26:33 +0100

    Added external links to social icons on home page and added noreferrer to links in MenuItems.js

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -5,31 +5,49 @@ import {Link} from 'react-router-dom';
 
 function HomePage() {
     return (
-        <div className="HomePage">
-            <header className="hero">
-                <h1 className="hero-text">
-                    Hi, I am 
-                    <span> Adrian Looney</span>
-                </h1>
-                <p className="h-sub-text"> 
-                    Random placeholder text. Random placeholder text. Random placeholder text. Random placeholder text.
-                    Random placeholder text.Random placeholder text.Random placeholder text.Random placeholder text.
-                </p>
-                <div className="icons">
-                    <Link className="icon-holder">
-                        <FontAwesomeIcon icon={faInstagram} className="icon ig" />
-                    </Link> 
-                    <Link className="icon-holder">
-                    <a href="https://www.apple.com" target="_blank"><FontAwesomeIcon icon={faGithub} className="icon gh"/>
-                        </a>
-                    </Link>
-                    <Link className="icon-holder">
-                        <FontAwesomeIcon icon={faLinkedin} className="icon li" />
-                    </Link>
-                </div>
-            </header>
-        </div>
-    )
+      <div className="HomePage">
+        <header className="hero">
+          <h1 className="hero-text">
+            Hi, I am
+            <span> Adrian Looney</span>
+          </h1>
+          <p className="h-sub-text">
+            Random placeholder text. Random placeholder text. Random placeholder
+            text. Random placeholder text. Random placeholder text.Random
+            placeholder text.Random placeholder text.Random placeholder text.
+          </p>
+          <div className="icons">
+            <Link className="icon-holder" target="_blank">
+            <a
+                href="https://www.instagram.com/adey1981/"
+                target="_blank"
+                rel="noreferrer"
+              >
+              <FontAwesomeIcon icon={faInstagram} className="icon ig" />
+            </a>
+            </Link>
+            <Link className="icon-holder" target="_blank">
+              <a
+                href="https://github.com/Jammerref2015"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <FontAwesomeIcon icon={faGithub} className="icon gh" />
+              </a>
+            </Link>
+            <Link className="icon-holder" target="_blank">
+              <a
+                href="https://www.linkedin.com/in/adrian-looney/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <FontAwesomeIcon icon={faLinkedin} className="icon li" />
+              </a>
+            </Link>
+          </div>
+        </header>
+      </div>
+    );
 }
 
 export default HomePage;
