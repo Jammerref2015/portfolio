5810316170d302e72f32cb7c8cbfd34dfd689f8a
Jammerref2015 <adrian.looney@gmail.com>
2021-09-08 16:03:57 +0100

    updated contact page

--- src/components/ContactItem.js
+++ src/components/ContactItem.js
@@ -8,7 +8,8 @@ function ContactItem({icon, text1, text2, title}) {
                 <div className="right-items">
                     <h6>{title}</h6>
                     <p>{text1}</p>
-                    <p>{text2}</p>
+                    <p>{<a href="mailto:adrian.looney@gmail.com">Send Email</a>
+}</p>
                 </div>
             </div>
         </div>
